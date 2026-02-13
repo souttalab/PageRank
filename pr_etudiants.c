c2597e810f7550cae5f773e0158758d0c19a8149
souttalab <outtalab.salaheddine@gmail.com>
2026-02-13 17:18:01 +0100

    Q7

--- pr_etudiants.c
+++ pr_etudiants.c
@@ -53,7 +53,7 @@ void printDigraph(DIGRAPH *g)
     }
 }
 
-void score(DIGRAPH *g, int k)
+/*void score(DIGRAPH *g, int k)
 {
     double *s = (double *)malloc(g->n * sizeof(double));
     double *s_k = (double *)malloc(g->n * sizeof(double));
@@ -99,7 +99,103 @@ void score(DIGRAPH *g, int k)
     printf(")\n");
     free(s);
     free(s_k);
+}*/
+int* compar(const void* i, const void* j, void* list){
+    double* liste =list;
+    const *j = j;
+    const *i = i;
+
 }
+void qsort_r(void base, size_t nmemb, size_t size,
+                  int (*compar)(const void , const void , void *),
+                  void *arg);
+
+void score(DIGRAPH *g)
+{
+    double *s = (double *)malloc(g->n * sizeof(double));
+    double *s_k = (double *)malloc(g->n * sizeof(double));
+    double epsilon = 1.0e-10; 
+    double alpha = 0.9;       
+
+    for (int i = 0; i < g->n; i++)
+    {
+        s[i] = 1.0 / g->n;
+    }
+
+    int iter = 0;
+    while (1) 
+    {
+        iter++;
+        double qabs = 0.0;
+        
+        for (int i = 0; i < g->n; i++)
+        {
+            if (g->nbSucc[i] == 0)
+            {
+                qabs += s[i] / g->n;
+            }
+            s_k[i] = 0.0; 
+        }
+
+        for (int i = 0; i < g->n; i++)
+        {
+            s_k[i] += qabs * alpha + (1.0 - alpha) / g->n;
+            
+            for (int j = 0; j < g->nbSucc[i]; j++)
+            {
+                s_k[g->succ[i][j]] += (s[i] / g->nbSucc[i]) * alpha;
+            }
+        }
+
+        double max_diff = 0.0;
+        for (int i = 0; i < g->n; i++) {
+            double diff = fabs(s_k[i] - s[i]);
+            if (diff > max_diff) max_diff = diff;
+        }
+
+        double *temp = s;
+        s = s_k;
+        s_k = temp;
+
+        
+        if (max_diff < epsilon) break;
+    }
+
+    printf("Convergence atteinte après %d itérations.\n", iter);
+
+
+    int *indices = (int *)malloc(g->n * sizeof(int));
+    for(int i=0; i<g->n; i++) indices[i] = i;
+
+
+    for (int i = 0; i < g->n - 1; i++)
+    {
+        int max_idx = i;
+        for (int j = i + 1; j < g->n; j++)
+        {
+            if (s[indices[j]] > s[indices[max_idx]])
+            {
+                max_idx = j;
+            }
+        }
+
+        int temp = indices[i];
+        indices[i] = indices[max_idx];
+        indices[max_idx] = temp;
+    }
+
+    printf("Top 5 pages:\n");
+    int limit = (g->n < 5) ? g->n : 5; 
+    for (int i = 0; i < limit; i++)
+    {
+        printf("Page %d : %f\n", indices[i], s[indices[i]]);
+    }
+
+    free(indices);
+    free(s);
+    free(s_k);
+}
+    
 
 int main()
 {
@@ -107,6 +203,6 @@ int main()
     DIGRAPH *g = readDigraph(fp);
     fclose(fp);
     printDigraph(g);
-    score(g, 4);
+    score(g);
     return 0;
 }
